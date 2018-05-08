5e89ee63b90e8401b6b66e57fe6807a75debd132
Mohammed Chisti <93mchisti@gmail.com>
2018-05-07 22:00:42 -0400

    Finished login design

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Login extends Component {
   render() {
     return (
-      <div>
+      <div className='login'>
         <h1>Welcome Back</h1>
 
         <input type='text' placeholder='username'/>
