075f73dc1b58837ee0f0070eb3173f1a5497f107
Mohammed Chisti <93mchisti@gmail.com>
2018-05-12 19:47:38 -0400

    Changes to routes

--- src/routes.js
+++ src/routes.js
@@ -10,14 +10,20 @@ export default (
     <div>
       <Switch>
         <Route path='/signup' component={ Signup } render={ () => autoHome() }/>
-        <Route path='/login' component={ Login } render={ () => autoHome() }/>
-        <Route path='/chats' component={ ChatContainer } render={ () => requireAuth() }/>
+        <Route path='/login' render={ () => loggedIn(<Redirect to="/chats"/>) }/>
+        <Route path='/chats' render={ () => loggedIn(<ChatContainer/>) }/>
         <Route path='/logout' render={ () => logout() }/>
         <Route path='/' component={ ChatContainer } render={ () => requireAuth() }/>
       </Switch>
     </div>
   </BrowserRouter>
 )
+
+function loggedIn(component) {
+  if(sessionStorage['jwt']) return component
+
+  return <Login/>
+}
 
 function requireAuth() {
   if(!sessionStorage['jwt']) <Redirect to="/login"/>
