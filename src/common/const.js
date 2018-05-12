5088175cf1ab3b9b41a2555a102f92946d82b0cf
Mohammed Chisti <93mchisti@gmail.com>
2018-05-12 12:24:25 -0400

    Finished redux boilerplates. Need start working on refractoring while working on login/signup on new branch.

--- src/common/const.js
+++ src/common/const.js
@@ -1,6 +1,6 @@
 export const CONST = {
-  URL: 'https://newsapi.org/v2/',
-  API_KEY: '581e9a12b49747c09aee13475cd00ff9',
+  URL: 'https://meld-api.herokuapp.com/',
+  URL_T: 'http://localhost:3005',
 
   FETCH_USER: 'FETCH_USER',
   LOGIN_OR_SIGNUP: 'LOGIN_OR_SIGNUP'
