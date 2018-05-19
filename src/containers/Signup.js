58aa16cd962229c49bf7b7cc7a3de8bb89d34709
Mohammed Chisti <93mchisti@gmail.com>
2018-05-19 00:11:27 -0400

    Added redux to signup

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { Link, withRouter } from 'react-router-dom'
+
+import { signup } from '../actions/userActions'
 
 class Signup extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      username: '',
+      firstname: '',
+      lastname: '',
+      password: '',
+      password_confirmation: ''
+    }
+
+    this.onChange = this.onChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  onChange(e) {
+    let state = this.state
+    let field = e.target.name
+
+    state[field] = e.target.value
+    this.setState({ state })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+
+    this.props.login({ user: this.state })
+    this.props.history.push('/')
+  }
+
   render() {
     return (
-      <form className='signup'>
+      <form className='signup' onSubmit={ this.handleSubmit }>
         <h1>Join Us</h1>
 
         <input type='text' placeholder='username'/>
@@ -22,4 +56,10 @@ class Signup extends Component {
   }
 }
 
-export default Signup
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({
+    signup: signup
+  }, dispatch)
+}
+
+export default connect(null, null)(withRouter(Signup))
